94bf1262e62eeed686067064c321d11de5af600c
BEHRUZBEK TURSUNOV <alexmeyson966@gmail.com>
2022-10-07 22:06:55 +0500

    javascript

---
+++ js/app.js
@@ -0,0 +1,12 @@
+alert ('hush kelibsiz')
+let ism = prompt('ismingizni kiriting')
+console.log(ism);
+
+let firstName = prompt('familiyangizni kiriting')
+console.log(firstName);
+
+let sam = prompt('telefon nomerizni kiriting', '+998')
+console.log(sam);
+
+let box = confirm('raxmad')
+console.log(box);
